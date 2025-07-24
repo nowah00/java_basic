933a471e9d13a1698e688a61b557d51983dbc10c
nowah00 <hawon2k@gmail.com>
2025-07-25 08:40:29 +0900

    practice6

--- src/jungol/입력/Main.java
+++ src/jungol/입력/Main.java
@@ -2,6 +2,8 @@ package jungol.입력;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 //입력 - 연습문제1
@@ -28,20 +30,34 @@ public class Main {
 //        System.out.printf("%d + %d = %d", a, b, result);
 
 //입력 - 연습문제4
-        int a = 5;
-        int b = 2;
-        double pie =3.140000;
-        double result1 = a * b * pie;
-        double result2 = a * a * pie;
-
-        System.out.printf("원주 = %d * %d * %s = %s\n", a, b, pie, result1);
-        System.out.printf("넓이 = %d * %d * %s = %s", a, a, pie, result2);
-
-        //  왜 계속 자꾸 안되는거야!!!!
-
+//        int a = 5;
+//        int b = 2;
+//        double pie =3.140000;
+//        double result1 = a * b * pie;
+//        double result2 = a * a * pie;
+//
+//        System.out.printf("원주 = %d * %d * %s = %s\n", a, b, pie, result1);
+//        System.out.printf("넓이 = %d * %d * %s = %s", a, a, pie, result2);
 
 //입력 - 연습문제5
+//        float x= 1.2340f;
+//        float y= 10.3459f;
+//        System.out.printf("전체 7자리로 맞추고 소수 4자리까지 출력\n");
+//        System.out.printf("x =  %.4f\n",x);
+//        System.out.printf("y = %.4f\n\n",y);
+//        System.out.printf("소수 2자리까지 출력(반올림)\n");
+//        System.out.printf("x = %.2f\n",x);
+//        System.out.printf("y = %.2f\n",y);
+
 //입력 - 연습문제6
+        int num1, sum;
+        Scanner input= new Scanner(System.in);
+
+        System.out.println("당신의 나이는 몇 살입니까");
+        num1 = input.nextInt();
+        sum = num1;
+        System.out.printf("당신의 나이는 %d살이군요", sum);
+
 //입력 - 연습문제7
 //입력 - 연습문제8
 //입력 - 연습문제9
