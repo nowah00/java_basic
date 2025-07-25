9ea423b168d390f78b6ec9e7cdf511d6c7837f3d
nowah00 <hawon2k@gmail.com>
2025-07-25 15:53:49 +0900

    good

--- src/Day03/OperationEx01.java
+++ src/Day03/OperationEx01.java
@@ -57,7 +57,48 @@ public class OperationEx01 {
         z = ++x1 + y1++;
         System.out.println("z : " + z + " y1 : " + y1);
 
-    
+    // 산술연산자 +, -, *, /, %(나머지)
+        byte v1 = 10;
+        byte v2 = 4;
+        int v3 = 5;
+        long v4 = 10L;
+
+        int result4 = v1 + v2;      // 모든 피연산자는 int 타입으로 자동변환 후 연산
+        System.out.println(result4);
+
+        long result5 = v1 + v2 - v4;    // 모든 피연산자는 가장 큰 long 타입으로 프로모션된 후 연산
+        System.out.println(result5);
+
+        double result6 = (double) v1/v2;        // double 로 타입 강제 변환 후 연산
+
+        int result7 = v1 % v2;
+        System.out.println(result7);
+
+        if (v1 % v2 == 0){
+            System.out.println("나머지는 짝수");
+        }else {
+            System.out.println("");
+        }
+
+
+        int apple = 1;
+        int totalPieces = apple * 10;
+        int number = 7;
+
+        int result = totalPieces - number;
+        System.out.println("10조각에서 남은 조각 : " + result);
+        System.out.println("사과 한개에서 남은 양 : " + result/10.0);
+
+
+        int k = 5;
+        int result8 = k%2;      // %는 2로 5를 나눈 나머지 값
+        System.out.println(result8);
+
+
+
+// 비교연산자 동등 (==, !=), 크기 (<, <=, >, >=) 를 평가해서 boolean 타임인 true, false 를 산출한다
+
+
 
 
 
