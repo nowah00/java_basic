bbb9789860575655a58d57a060e191200643f79d
nowah00 <hawon2k@gmail.com>
2025-07-25 14:22:56 +0900

    in_End

--- src/jungol/입력/자가진단.java
+++ src/jungol/입력/자가진단.java
@@ -1,5 +1,7 @@
 package jungol.입력;
 
+import java.util.Scanner;
+
 public class 자가진단 {
     public static void main(String[] args) {
 // 자가진단1
@@ -34,12 +36,47 @@ public class 자가진단 {
 //        System.out.printf("%d * %f = %f", x, y, sum);
 
 // 자가진단5
+//        float yd = 2.1f;
+//        float in = 10.5f;
+//        float yc = yd * 91.44f;
+//        float ic = in * 2.54f;
+//
+//        System.out.printf("%.1fyd = %.1fcm\n", yd, yc);
+//        System.out.printf("%.1fin = %.1fcm", in, ic);
 
+// 자가진단6
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.println("height = ");
+//        int h = in.nextInt();
+//
+//        System.out.printf("Your height is %dcm", h);
 
+// 자가진단7
+        Scanner in = new Scanner(System.in);
 
+//        int a = in.nextInt();
+//        int b = in.nextInt();
+//        int result1, result2;
+//
+//        result1 = a * b;
+//        result2 = a / b;
+//
+//        System.out.printf("%d * %d = %d\n", a, b, result1);
+//        System.out.printf("%d / %d = %d", a, b, result2);
 
+// 자가진단8
+//        double a = in.nextDouble();
+//        double b = in.nextDouble();
+//        String c = in.next();
+//
+//        System.out.printf("%.2f\n%.2f\n%s", a, b, c);
 
-
-
+// 자가진단9
+//        double a = in.nextDouble();
+//        double b = in.nextDouble();
+//        double c = in.nextDouble();
+//
+//        System.out.printf("%.3f\n%.3f\n%.3f", a, b, c);
     }
 }
--- src/jungol/입력/형성평가.java
+++ src/jungol/입력/형성평가.java
@@ -38,12 +38,12 @@ public class 형성평가 {
 //        System.out.println(a + "+" + b + "+" + c + "=" + sum);
 
 // 형성평가5
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("yard?");
-        double a = input.nextDouble();
-        double result = a * 91.44;
-
-        System.out.printf("%.1fyard = %.1fcm", a, result);
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("yard?");
+//        double a = input.nextDouble();
+//        double result = a * 91.44;
+//
+//        System.out.printf("%.1fyard = %.1fcm", a, result);
     }
 }
