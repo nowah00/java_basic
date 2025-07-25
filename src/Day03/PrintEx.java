66f482b53d8cfbf9c04deeb0fdc530c348e0f5fd
nowah00 <hawon2k@gmail.com>
2025-07-25 12:28:22 +0900

    o

--- src/Day03/PrintEx.java
+++ src/Day03/PrintEx.java
@@ -16,5 +16,20 @@ public class PrintEx {
         System.out.printf("%-6d \n", 123);
         //정수 000123을 printf() 출력하고싶다. 6자리 정수 출력.
         System.out.printf("%06d \n", 123);
+
+        System.out.println(" ===========실수 표현===========");
+
+        // 전수 7자리 = 소수점 = 소수2자리 . 왼쪽 빈자리 공백 ____123.45
+        System.out.printf("%10.2f\n",123.45);
+
+        System.out.println(" ===========문자열 표현===========");
+
+        // 문자열은 %s 포맷       abc 출력
+        System.out.printf("%s\n", "abc");
+        // 문자열은 %s 포맷       ___abc 출력
+        System.out.printf("%6s\n", "abc");
+
+        // 특수 문자 \t, \n %%
+        System.out.printf("소금물의 농고 %% %d\n", 35);
     }
 }
