93cbc56353ac33ad497c0a080c50e3e543981b9a
nowah00 <hawon2k@gmail.com>
2025-07-25 09:18:44 +0900

    o

--- src/jungol/입력/형성평가.java
+++ src/jungol/입력/형성평가.java
@@ -2,9 +2,22 @@ package jungol.입력;
 
 public class 형성평가 {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int c = 30;
-        System.out.printf("%d + %d = %d", a,b,c);
+// 형성평가1
+//        int a = 10;
+//        int b = 20;
+//        int c = 30;
+//        System.out.printf("%d + %d = %d", a,b,c);
+
+// 형성평가2
+        float a = 80.5f;
+        float b = 22.34f;
+        float c = 102.84f;
+
+        System.out.printf("%.2f     %.2f     %.2f",a,b,c);
+
+
+
+
+
     }
 }
