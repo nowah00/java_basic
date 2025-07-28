64767a22c4031f29d8ad94bad2cefda43b8d14d4
nowah00 <hawon2k@gmail.com>
2025-07-29 08:46:11 +0900

    08:46

---
+++ src/jungol/반복제어문3/반복제어문3_연습문제7.java
@@ -0,0 +1,12 @@
+package jungol.반복제어문3;
+
+import java.util.Scanner;
+
+public class 반복제어문3_연습문제7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+    }
+}
+
