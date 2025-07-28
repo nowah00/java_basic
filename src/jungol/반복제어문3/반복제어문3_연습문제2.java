06844b750cbe7814067c11c266f0382f5411b007
nowah00 <hawon2k@gmail.com>
2025-07-28 17:31:07 +0900

    P_2

---
+++ src/jungol/반복제어문3/반복제어문3_연습문제2.java
@@ -0,0 +1,25 @@
+package jungol.반복제어문3;
+
+import java.util.Scanner;
+
+public class 반복제어문3_연습문제2 {
+    public static void main(String[] args) {
+
+        for (int i=1; i<=3; i++){
+                if (i == 1){
+                    System.out.println("* ");
+                } else if (i == 2) {
+                    System.out.println("** ");
+                } else {
+                    System.out.println("***");
+                }
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ src/jungol/반복제어문3/반복제어문3_연습문제3.java
@@ -0,0 +1,15 @@
+package jungol.반복제어문3;
+
+public class 반복제어문3_연습문제3 {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+    }
+}
