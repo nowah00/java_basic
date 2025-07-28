f1b18b904edeedca0ea96895ff02dbba5dd973b5
nowah00 <hawon2k@gmail.com>
2025-07-28 17:25:12 +0900

    P_1

--- src/jungol/반복제어문3/반복제어문3_연습문제1.java
+++ src/jungol/반복제어문3/반복제어문3_연습문제1.java
@@ -8,17 +8,17 @@ public class 반복제어문3_연습문제1 {
 // 1부터 차례로 누적하여 합을 구하다가 합이 입력받은 수를 넘으면 중단하고 마지막으로 더해진 값과 그 때까지의 합을 출력하는 프로그램을 작성하시오.
         Scanner in = new Scanner(System.in);
 
-        int number = in.nextInt();
-        int count = 0;
+        int number = in.nextInt(); // 이게 입력한 수야.
+        int count = 0; // 이게 총합이고?
 
-        for (int i=1; i<=number; i++) {
-            count = count + i;
-            if (count > number) {
-                System.out.print(i + " ");
-                break;
+        for (int i=1; i<=number; i++) { // 입력한 수까지 반복되게 만들고?
+            count = count + i; // 총합을 구해.
+            if (count > number) { // 총합이 입력한 수를 넘어갔을 때
+                System.out.print(i + " "); // 그 때의 진행 값 i 를 출력하고
+                break; // 멈춰.
             }
         }
-        System.out.print(count);
+        System.out.print(count); // 총합까지~
 
 
 
