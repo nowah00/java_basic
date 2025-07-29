bd7eb43ff8adaa786e12fa7bece5beff0c8e6774
nowah00 <hawon2k@gmail.com>
2025-07-29 09:04:11 +0900

    09:03

--- src/jungol/반복제어문3/반복제어문3_연습문제7.java
+++ src/jungol/반복제어문3/반복제어문3_연습문제7.java
@@ -4,9 +4,21 @@ import java.util.Scanner;
 
 public class 반복제어문3_연습문제7 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
 
+        int a = 97;
+        int b = 1;
 
+        for (int i=1; i<=4; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print((char)a + " ");
+                a++;
+            }
+            for (int k=1; k<=5-i; k++){
+                System.out.print(b + " ");
+                b++;
+            }
+            System.out.println();
+        }
     }
 }
 
