9888ee1def1f45dda8dbb2d97436e70e144af119
nowah00 <hawon2k@gmail.com>
2025-07-30 16:57:19 +0900

    hu

--- src/jungol/배열1/배열1_연습문제09.java
+++ src/jungol/배열1/배열1_연습문제09.java
@@ -7,7 +7,7 @@ public class 배열1_연습문제09 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int[] arr = {95, 75, 85, 100, 50};
+        int[] arr = {95, 75, 85, 100, 50}; // 배열의 생성 및 초기화 한번에 정리
 
         Arrays.sort(arr);
 
