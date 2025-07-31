5a1fdafde467f1d9f893c816979b3ba5ddb1f61c
nowah00 <hawon2k@gmail.com>
2025-07-31 12:37:08 +0900

    12:37

---
+++ src/javabagic_02/Day07/DoubleArrayEx01.java
@@ -0,0 +1,46 @@
+package javabagic_02.Day07;
+
+public class DoubleArrayEx01 {
+
+    public static void main(String[] args) {
+
+    // 학생 3명의 국어, 영어, 수학 점수를 관리하는 이차원 벼열을 만들어라
+        int[][] scores = new int [3][3];
+
+    // 1번 학생의 국어:90 영어:50 수학:80을 저장하고 출력하세요.
+        scores[0][0] = 90; // 국어
+        scores[0][1] = 50; // 영어
+        scores[0][2] = 80; // 수학
+
+    // 2번 학생의 국어:80 영어:100 수학:90 을 저장하고 출력하세요.
+        scores[1][0] = 90;
+        scores[1][1] = 100;
+        scores[1][2] = 50;
+
+    // 3번 학생의 국어:100 영어:90 수학:90 을 저장하고 출력하세요.
+        scores[2][0] = 100;
+        scores[2][1] = 90;
+        scores[2][2] = 90;
+
+    // 각 학생들의 국어, 영어, 수학 점수를 학생별로 출력하세요.
+        int s = 1;
+        int total = 0;
+        int avg = 0;
+        int count = 0;
+
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(s + "번째 학생");
+            for (int j = 0; j < scores.length; j++) {
+                total += scores[i][j];
+                System.out.println(scores[i][j]);
+                count++;
+            }
+            s++;
+            avg = total / count;
+            System.out.printf("total : %d \navg : %d\n\n", total, avg);
+            total = 0;
+            avg = 0;
+        }
+
+    }
+}
