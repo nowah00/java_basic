8b89b6b0823338efbb599b7be4bca6ce4aaae459
nowah00 <hawon2k@gmail.com>
2025-08-01 17:49:16 +0900

    17:41

---
+++ src/jungol/배열2/배열2_연습문제07.java
@@ -0,0 +1,10 @@
+package jungol.배열2;
+
+import java.util.Scanner;
+
+public class 배열2_연습문제07 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+    }
+}
