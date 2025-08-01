c64458783d3751bcd461bdcf38ea9ae46a1236cb
nowah00 <hawon2k@gmail.com>
2025-08-01 15:38:19 +0900

    15:38

---
+++ src/jungol/문자열_복습하자/문자열_자가진단1.java
@@ -0,0 +1,19 @@
+package jungol.문자열_복습하자;
+
+import java.util.Scanner;
+
+public class 문자열_자가진단1 {
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        while(true){
+            System.out.printf("ASCII code =? ");
+         int numberCode = in.nextInt();
+
+         if(numberCode>=33 && numberCode<=127){
+             System.out.print((char)numberCode);
+             System.out.println();
+         } else break;
+        }
+    }
+}
