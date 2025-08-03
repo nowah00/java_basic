044397d284430118da4f1af417e7170513f6d233
nowah00 <hawon2k@gmail.com>
2025-08-04 08:48:45 +0900

    UU

--- src/jungol/배열2/배열2_연습문제07.java
+++ src/jungol/배열2/배열2_연습문제07.java
@@ -4,19 +4,18 @@ import java.util.Scanner;
 
 public class 배열2_연습문제07 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
         int[][] number = new int[5][5];
-        number[0][0] = 1;
 
-        for (int i = 1; i < 5; i++) {
-            for (int j = 1; j < 5; j++) {
-                    number[i][j] = number[i - 1][j] + number[i - 1][j - 1];
+        for (int i = 0; i < 5; i++) {
+            number[i][0] = 1;
+            for (int j = 1; j <= i; j++) {
+                number[i][j] = number[i - 1][j] + number[i - 1][j - 1];
             }
         }
+
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j <= i; j++) {
-                System.out.print(number[0][0] + " ");
+                System.out.print(number[i][j] + " ");
             }
             System.out.println();
         }
---
+++ src/jungol/배열2/배열2_형성평가04.java
@@ -0,0 +1,14 @@
+package jungol.배열2;
+
+import java.util.Scanner;
+
+public class 배열2_형성평가04 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+
+
+
+    }
+}
