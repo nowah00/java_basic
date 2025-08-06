6e71f6d9935f7ca7878539c5c06b37f66a66aea6
nowah00 <hawon2k@gmail.com>
2025-08-06 17:29:57 +0900

    Help Me

--- src/javabasic_02/day11/test/class03/prob1/Prob1.java
+++ src/javabasic_02/day11/test/class03/prob1/Prob1.java
@@ -1,16 +1,14 @@
-//package javabasic_02.day11.test.class03.prob1;
-//
-//public class Prob1 {
-//
-//	public static void main(String[] args) {
-//
-//		Emp emp = new Emp();
-//		emp.setId("12345");
-//		emp.setName("홍길동");
-//		emp.setBaseSalary(50000);
-//		System.out.println( emp.toString() );
-//		System.out.println( "봉급 = " + emp.getBaseSalary());
-//
-//	}
-//
-//}
+package javabasic_02.day11.test.class03.prob1;
+
+public class Prob1 {
+
+	public static void main(String[] args) {
+
+		Emp emp = new Emp();
+		emp.setId("12345");
+		emp.setName("홍길동");
+		emp.setBaseSalary(50000);
+		System.out.println( emp.toString());
+		System.out.println( "봉급 = " + emp.getSalary( 30 ));
+	}
+}
