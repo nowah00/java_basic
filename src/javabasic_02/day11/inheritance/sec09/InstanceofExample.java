b1bbc28ff10ab81cc58bcf20644010885242f56b
nowah00 <hawon2k@gmail.com>
2025-08-07 16:28:53 +0900

    Help Me

--- src/javabasic_02/day11/inheritance/sec09/InstanceofExample.java
+++ src/javabasic_02/day11/inheritance/sec09/InstanceofExample.java
@@ -3,14 +3,20 @@ package javabasic_02.day11.inheritance.sec09;
 public class InstanceofExample {
 	//main() 메소드에서 바로 호출하기 위해 정적 메소드 선언
 	public static void personInfo(Person person) {
-
+		if (person instanceof Student student) {
+			System.out.println(" 학번 : " + student.studentNo);
+			student.study();
+		}
+		System.out.println("이름 : " + person.name);
+		person.walk();
 	}
 
 	public static void main(String[] args) {
 		//Person 객체를 매개값으로 제공하고 personInfo() 메소드 호출
-
-
+		Person person1 = new Person("신세계");
+		personInfo(person1);
 		//Student 객체를 매개값으로 제공하고 personInfo() 메소드 호출
-
+		Person p2 = new Student("홍길동", 100000);
+		personInfo(p2);
 	}
 }
