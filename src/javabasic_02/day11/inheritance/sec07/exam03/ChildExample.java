57572c7d8efa1ba5c0665cf6d3988ad7f49ced55
nowah00 <hawon2k@gmail.com>
2025-08-07 14:35:33 +0900

    Help Me

--- src/javabasic_02/day11/inheritance/sec07/exam03/ChildExample.java
+++ src/javabasic_02/day11/inheritance/sec07/exam03/ChildExample.java
@@ -1,17 +1,25 @@
-package javabasic_02.day11.inheritance.sec07.exam03;
-
-public class ChildExample {
-	public static void main(String[] args) {
-		//객체 생성 및 자동 타입 변환
-
-		
-		//Parent 타입으로 필드와 메소드 사용
-
-		
-		//강제 타입 변환
-
-		
-		//Child 타입으로 필드와 메소드 사용
-
-	}
-}
+//package javabasic_02.day11.inheritance.sec07.exam03;
+//
+//import javabasic_02.day11.inheritance.sec07.exam02.Child;
+//import javabasic_02.day11.inheritance.sec07.exam02.Parent;
+//
+//public class ChildExample {
+//	public static void main(String[] args) {
+//		//부모타입으로 자식객체를 생성하세요.
+//		Parent parent = new Child();
+//
+//		//Parent 타입으로 필드와 메소드 사용
+//		parent.field1 = "data1111";
+//		parent.method1();
+//		parent.method2();
+//
+//		//강제 타입 변환
+//		Child child = (Child) parent;
+//		child.field2 = "data22222";
+//		child.method3();
+//
+//
+//		//Child 타입으로 필드와 메소드 사용
+//
+//	}
+//}
