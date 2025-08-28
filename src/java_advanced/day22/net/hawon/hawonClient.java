c841ce09ef283234edea91f5786185485dd61b8e
nowah00 <hawon2k@gmail.com>
2025-08-28 12:19:02 +0900

    day23_am Commit

---
+++ src/java_advanced/day22/net/hawon/hawonClient.java
@@ -0,0 +1,18 @@
+package java_advanced.day22.net.hawon;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class hawonClient {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost",50005);
+            System.out.println("[클라이언트] 연결 완료");
+
+            socket.close();
+            System.out.println("[클라이언트] 연결 종료");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
